e6563973c4fe50745bf35e5e47672cf585efa8a6
eobukhova <eobukhova@samsonpost.ru>
2026-01-15 16:13:01 +0300

    absolute paths

--- src/masonry.js
+++ src/masonry.js
@@ -19,31 +19,9 @@ let masonryLayout = undefined;
 function initMasonry() {
   masonryLayout = new Masonry('.masonry', {
     itemSelector: '.masonry-card',
-    // columnWidth: 400,
     columnWidth: '.masonry .small',
-    // percentPosition: true,
     gutter: 30,
   });
 }
-
-// const allImgs = document.querySelectorAll('.masonry .masonry-card img');
-// let loadedCount = 0;
-//
-// allImgs.forEach((img) => {
-//   if (img.complete) {
-//     loadedCount++;
-//   } else {
-//     img.addEventListener('load', () => {
-//       loadedCount++;
-//     });
-//   }
-// });
-
-// const checkAndInit = setInterval(() => {
-//   if (loadedCount === allImgs.length) {
-//     clearInterval(checkAndInit);
-//     initMasonry()
-//   }
-// }, 50);
 
 export { masonryLayout };
