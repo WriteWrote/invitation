e95e22106a2dead822542610d70ca4af8b5a9540
eobukhova <eobukhova@samsonpost.ru>
2026-07-13 15:44:55 +0300

    опрос таблицы с рассадкой гостей

--- src/guestmap.js
+++ src/guestmap.js
@@ -1,7 +1,7 @@
 console.log("Heloo")
 // выгрузка данных из таблицы
 const SHEET_ID = '1qq9H3re_5A7QC0RRX2OimozcSEMcS3MgwdO0qBQ1LEY';
-const RANGE = 'Лист1!A1:B20';
+const RANGE = 'Лист1!A:B';
 const API_KEY = 'AIzaSyA8QmvjZbf_fkRHKcF6Q248olNwAjmDYk8';
 
 fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`)
@@ -10,4 +10,6 @@ fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}
     const rows = data.values;
 
     console.log(rows);
-  });
+  })
+  .catch(error => console.error('Ошибка:', error))
+;
