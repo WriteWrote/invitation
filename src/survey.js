2a068f1610ed682af0fa7e0ed1f1cd03eb0ba08d
WriteWrote <obukhova.elina@yandex.ru>
2026-08-16 13:31:14 +0300

    метка utm_source

--- src/survey.js
+++ src/survey.js
@@ -93,22 +93,16 @@ function sendSurveyAnswerToGoogleForm() {
 }
 
 function saveUtmToSession() {
-    const urlParams = new URLSearchParams(window.location.search);
-    console.log(window.location.search);
-    console.log(JSON.stringify(urlParams));
-    let utmData = {};
+    //"?utm_source=dv"
+  const urlParams = new URLSearchParams(window.location.search);
 
-    for (let [key, value] of urlParams) {
-        if (key.startsWith('utm_')) {
-            utmData[key] = value;
-        }
+  for (let param of urlParams.split('&')) {
+    let key, value - param.split('?');
+    console.log(key + " - " + value);
+    if (key.startsWith('?utm_')) {
+      sessionStorage.setItem(key.replace('?', ''), value);
     }
-    console.log(utm_data);
-    // Если метки найдены, сохраняем их в сессию
-    if (Object.keys(utmData).length > 0) {
-        sessionStorage.setItem('utm_data', JSON.stringify(utmData));
-        console.log('UTM-метки сохранены в сессии');
-    }
+  }
 }
 
 // Вызываем при загрузке страницы
